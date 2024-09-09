ceb0fb5518f6b2a6cfd2b9d79e6109c1ea1c8772
LeeHungQNG <builequochung3004@gmail.com>
2024-09-09 20:58:20 +0700

    func component

---
+++ src/examples/FunctionComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const FunctionComponent = () => {
+  const name = 'LeeHung';
+  return <div>FunctionComponent with {name}</div>;
+};
+
+export default FunctionComponent;
