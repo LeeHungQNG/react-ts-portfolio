135e17b1a1c393943077389668d604bc7c0982f1
LeeHungQNG <builequochung3004@gmail.com>
2024-09-10 17:04:16 +0700

    convert html template to react

--- src/examples/FunctionComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const FunctionComponent = () => {
-  const name123 = 'LeeHung';
-  return <div>FunctionComponent with {name123}</div>;
-};
-
-export default FunctionComponent;
